807db3c5f70e31f9ec86732d254aae19dc20cbfe
Ryan Hall <hall.ryan.r@gmail.com>
2019-02-19 11:51:27 -0500

    add truffle-assertions helper and write tests for #applyTo

--- test/Scholarship.js
+++ test/Scholarship.js
@@ -8,13 +8,15 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 contract("Scholarship", accounts => {
 
-  let scholarship;
-
-  before(async () => {
-    scholarship = await Scholarship.new(10, "test instructions", "test school", "test course", { from: accounts[1] });
-  })
-
   describe("#constructor", () => {
+    // Setup
+    let scholarship;
+    before(async () => {
+      scholarship = await Scholarship.new(10, "test instructions", "test school", "test course", { from: accounts[1] });
+      console.log(await scholarship.startedOn());
+      console.log(await scholarship.daysToComplete());
+    })
+    // Tests
     it("should be sponsored by transaction author", async () => {
       assert.equal(await scholarship.sponsor(), accounts[1]);
     });
@@ -33,10 +35,13 @@ contract("Scholarship", accounts => {
   })
 
   describe("#applyTo", () => {
+    // Setup
+    let scholarship;
     before(async () => {
+      scholarship = await Scholarship.new(10, "test instructions", "test school", "test course", { from: accounts[1] });
       await scholarship.applyTo("test link 2", { from: accounts[2] });
       await scholarship.applyTo("test link 3", { from: accounts[3] });
-    });
+    // Tests
     it("should add the application link to the mapping", async () => {
       assert.equal(await scholarship.applications(accounts[2]), "test link 2");
       assert.equal(await scholarship.applications(accounts[3]), "test link 3");
@@ -49,9 +54,15 @@ contract("Scholarship", accounts => {
   });
 
   describe("#awardTo", () => {
+    // Setup
+    let scholarship;
     before(async () => {
+      scholarship = await Scholarship.new(10, "test instructions", "test school", "test course", { from: accounts[1] });
+      await scholarship.applyTo("test link 2", { from: accounts[2] });
+      await scholarship.applyTo("test link 3", { from: accounts[3] });
       await scholarship.awardTo(accounts[2], { from: accounts[1] });
     });
+    // Tests
     it("should not be invokable by anyone other than the sponsor", async () => {
       await truffleAssert.reverts(scholarship.awardTo(accounts[3], { from: accounts[2] }));
     });
@@ -64,20 +75,18 @@ contract("Scholarship", accounts => {
     });
   });
 
+  describe("#getApplications", () => {
+    // Setup
+    let scholarship;
+    before(async () => {
+      scholarship = await Scholarship.new(10, "test instructions", "test school", "test course", { from: accounts[1] });
+      await scholarship.applyTo("test link 2", { from: accounts[2] });
+      await scholarship.applyTo("test link 3", { from: accounts[3] });
+      await scholarship.awardTo(accounts[2], { from: accounts[1] });
+    });
+    // Tests
+  });
 
-  // it("should use the testing endpoint", async () => {
-  //   const endpoint = await ScholarshipManager.verificationEndpoint();
-  //   assert.equal("http://localhost:8080", endpoint)
-  // });
-  //
-  // it("should not have any scholarships to begin with", async () => {
-  //   const scholarships = await ScholarshipManager.getScholarships();
-  //   assert.equal(0, scholarships.length);
-  // });
-  //
-  // it("allows creation of scholarships", async () => {
-  //   await ScholarshipManager.create(10, "test instructions", "test school", "test course");
-  //   const scholarships = await ScholarshipManager.getScholarships();
-  //   assert.equal(1, scholarships.length);
-  // });
+
+
 });
